3800b156dd0ad73128b5b7e5a5a67dd32e63a1f7
qubin2025 <qqs3333@gmail.com>
2026-06-26 13:22:51 +0800

    fix: CORS开发模式开放所有来源

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors(isProduction ? (corsOrigin ? {
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 } : { origin: false }) : {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
